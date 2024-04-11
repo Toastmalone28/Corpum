c1d910ca254ab34726142e369a91ab45ff2ab66a
Toastmalone28 <hohmannjulius1@gmail.com>
2024-04-11 11:00:40 +0200

    Spawn grids für restlichen räume hinzugefügt

--- Assets/Scripts/Items/PlayerInteraction.cs
+++ Assets/Scripts/Items/PlayerInteraction.cs
@@ -29,13 +29,10 @@ public class PlayerInteraction : MonoBehaviour
         text.enabled = false;
         scene = SceneManager.GetActiveScene();
         uiController = UIManager.GetComponent<UserInterfaceController>();
-        //keybinds = GetComponent<Keybinds>();
     }
     void FixedUpdate()
-    //event hinzufügen für performance reasons
     {
         ItemCheck();
-        
         UpdateItemCooldown();
     }
 
@@ -49,7 +46,6 @@ public class PlayerInteraction : MonoBehaviour
         foreach (Card_Object card in GetComponent<PlayerStats>().activeCards.Container)
         {
             card.coolDownTimer -= Time.deltaTime;
-            //Debug.Log(card.coolDownTimer);
             UpdateUICooldown(card);
         }
         foreach (Card_Object card in GetComponent<PlayerStats>().passiveCards.Container)
@@ -87,18 +83,13 @@ public class PlayerInteraction : MonoBehaviour
         }
     }
 
-
-    //sauber machen
-    //sauber machen
-    //sauber machen
     private void UseActiveItem()
     {
         PlayerStats stats = GetComponent<PlayerStats>();
 
-        //Klasse Keybinds ist extrem langsam??
         if(stats.activeCards.Container.Count > 0)
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
+            if (Input.GetKeyDown(keybinds.active1))
             {
                 Card_Object card1 = stats.activeCards.Container[0];
                 Debug.Log("Input detected: 1");
