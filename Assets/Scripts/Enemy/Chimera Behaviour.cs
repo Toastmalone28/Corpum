0877945eafbc262efb209e2c95b7dbc23e8748b4
ErdbeerCream <tony.paetzel@edu.bib.de>
2024-05-02 10:17:16 +0200

    Boss Attacks

--- Assets/Scripts/Enemy/Chimera Behaviour.cs
+++ Assets/Scripts/Enemy/Chimera Behaviour.cs
@@ -1,33 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Pathfinding;
 using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
+using UnityEngine.ProBuilder;
 
 public class ChimeraBehaviour : EnemyBehaviour
 {
     public float swipeCooldown = 2f;
-    private bool swipeReady = true;
+    public float shootCooldown = 2f;
+    private bool attackReady = true;
+    public BossProjectile proj;
 
     // Update is called once per frame
     void Update()
     {
         distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-        if (distanceToPlayer < 6 && swipeReady)
+        if (distanceToPlayer < 6 && attackReady)
         {
             animator.SetBool("swipeAttackRange", true);
             
             StartCoroutine(SwipeTimer());
         }
-        if (distanceToPlayer >= 6)
+        else
         {
             animator.SetBool("swipeAttackRange", false);
         }
+
+        if (distanceToPlayer >= 6 && attackReady)
+        {
+            animator.SetBool("shootAttack", true);
+
+            StartCoroutine(ShootTimer());
+        }
+        else
+        {
+            animator.SetBool("shootAttack", false);
+        }
+    }
+
+    IEnumerator ShootTimer()
+    {
+        attackReady = false;
+        float timer = 0f;
+
+        while (timer < shootCooldown)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        GameObject.Instantiate(proj, this.transform.position, this.transform.rotation);
+        attackReady = true;
     }
 
     IEnumerator SwipeTimer()
     {
-        swipeReady = false;
+        attackReady = false;
         float timer = 0f;
 
         while (timer < swipeCooldown)
@@ -35,6 +65,6 @@ public class ChimeraBehaviour : EnemyBehaviour
             timer += Time.deltaTime;
             yield return null;
         }
-        swipeReady = true;
+        attackReady = true;
     }
 }
